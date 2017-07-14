acfa4f60e9d0831142fa4f8979ce453a3339fc4f
Sahil Kharya <kharyasahil@gmail.com>
2017-07-15 02:30:38 +0530

    details
    added more details and info of the song and also some improvements

--- main.js
+++ main.js
@@ -71,61 +71,105 @@ function updateCurrentTime() {
     $('.song-duration').text(duration); //show duration
 }
 
-// given the song names and other list in a array instead of defining a variable for each
-var fileNames = ['song1.mp3','song2.mp3','song3.mp3','song4.mp3'];
-var artistList = ['Artist #1', 'Artist #2', 'Artist #3', 'Artist #4'];
-var albumList = ['Badrinath ki Dulhania','Ok Jaanu','Befikre','Ae Dil Hai Mushkil'];
-var durationList = ['2:56','3:15','2:34','2:29'];
 
-function addSongNameClickEvent(songName,position) {
-    var id = '#song' + position;
+//Arrays and objects
+/*we use arrays for multiple list of songs so that we do not write the same code for each and every song repeatedly
+ a var 'song' having array[] which contains objects(object are put inside {curly braces})
+    objects cnsists of 'key':'value' pairs, there can be many key value pairs inside an object
+    here we have 5 pairs*/
+var songs = [{
+        'name': 'Badri Ki Dulhania (Title Track)',
+        'artist': 'Neha Kakkar, Monali Thakur, Ikka Singh, Dev Negi',
+        'album': 'Badrinath ki Dulhania',
+        'duration': '2:56',
+        'fileName': 'song1.mp3',
+        'image': 'song1.jpg'
+    }, //objects are seperated by a comma
+    {
+        'name': 'Humma Song',
+        'artist': 'Badshah, Jubin Nautiyal, Shashaa Tirupati',
+        'album': 'Ok Jaanu',
+        'duration': '3:15',
+        'fileName': 'song2.mp3',
+        'image': 'song2.jpg'
+    },
+    {
+        'name': 'Nashe Si Chadh Gayi',
+        'artist': 'Arijit Singh',
+        'album': 'Befikre',
+        'duration': '2:34',
+        'fileName': 'song3.mp3',
+        'image': 'song3.jpg'
+    },
+    {
+        'name': 'The Breakup Song',
+        'artist': 'Nakash Aziz, Arijit Singh, Badshah, Jonita Gandhi',
+        'album': 'Ae Dil Hai Mushkil',
+        'duration': '2:29',
+        'fileName': 'song4.mp3',
+        'image': 'song4.jpg'
+    }];
+
+function changeCurrentSongDetails(songObj) {
+    $('.current-song-image').attr('src','img/' + songObj.image)
+    $('.current-song-name').text(songObj.name)
+    $('.current-song-album').text(songObj.album)
+}
+
+// we have taken the whole object(songObj) as a parameter inside our function
+function addSongNameClickEvent(songObj,position) {
+    var songName = songObj.fileName; // New Variable selecting fileName from the objects
+    var id = '#song' + position; 
     $(id).click(function() {
-        var audio = document.querySelector('audio');
+        var audio = document.querySelector('audio'); //'selected audio' tag
         var currentSong = audio.src;
+        
         /* 'if' - we first serch the song which is playing(.search)
-        (!= -1) means that the song.paused if not equal to -1(i.e false) i.e. we check whether the song.paused property is true, if it is not false (!= -1) then 'if' will get executed */
+        (!= -1) means that the song.paused if not equal to -1(i.e false) i.e. we check whether the song.paused property is true, if it is not false (!= -1) then 'if' will get executed 
+            i.e. 'if' will get executed if clicked on the same song which is being played or is in paused state*/
         if(currentSong.search(songName) != -1) {
             toggleSong();
         }
-        /* 'else' will execute when the 'if' function don't work */
+        /* 'else' will execute when the 'if' function don't work 
+            i.e. it will be executed on changing a song*/
         else {
-            audio.src = songName;
+            audio.src = songName; //
             toggleSong();
+            changeCurrentSongDetails(songObj); // Function Call
         }
     });
 }
-/*for loop - to add the function 'addSongNameClickEvent' to work on the list of all songs present in the library*/
-for (var i = 0; i < fileNames.length ; i++) {
-    addSongNameClickEvent(fileNames[i],i+1)
-} 
 
-//Arrays
-/*we use arrays for multiple list of songs so that we do not write the same code for each and every song repeatedly */
-var songList = ['Badri Ki Dulhania (Title Track)',
-                'Humma Song', 'Nashe Si Chadh Gayi', 'The Breakup Song']; 
+
 
 /* window.onload is a function which tells our script inside it to work when the window if completely loaded(including all the things)*/
 window.onload = function() {
-
+    changeCurrentSongDetails(songs[0]); //make first song (song[0]) as the default song whose details are to be shown when website is loaded 
+    
     /* "For Loop"
     (1) first we give the value from where it needs to start i.e. i=0
     (2) after that we give condition/ compare it , i.e. i < length of songlist(i < songList.length)
     (3) the it will run the complete function
     (4) At last it will increment the value after running thw complete function
     */
-    for(var i =0; i < songList.length;i++) {
-        var name = '#song' + (i+1);// giving name string+ value 
-        var song = $(name); //variable song now gets the value of name we need (first is song1)
-        //now we have searched the var song once in the html file and will find the given classes inside the "var song"
-        song.find('.song-name').text(songList[i]);
-        song.find('.song-artist').text(artistList[i]);
-        song.find('.song-album').text(albumList[i]); 
-        song.find('.song-length').text(durationList[i]); 
+    for(var i =0; i < songs.length;i++) {
+        var obj = songs[i];  // we Saved the song object in variable 'obj' ,  (songs is a var defined for array containing objects {line 80})
+        var name = '#song' + (i+1); // name of the song with its id number
+        var song = $(name);
+        // we selected the relevant object property from the object we have given above as - ".text(obj.property)", hum ise use karke property select/apply karte hai idhar par from the object
+        song.find('.song-name').text(obj.name);
+        song.find('.song-artist').text(obj.artist);
+        song.find('.song-album').text(obj.album);
+        song.find('.song-length').text(obj.duration);
+        addSongNameClickEvent(obj,i+1)   //passed a whole object as parameter of the function and also position of song  (Added a click event on each song)
     }
-    
-    updateCurrentTime();// start updating time from the beginning 
+     updateCurrentTime();// start updating time from the beginning 
     // set interval to update the time of the playing song
     setInterval(function() {
         updateCurrentTime();//update time in regular intervals
     },1000);
+    
+    $('#songs').DataTable({
+        paging: false
+    });
 }   
